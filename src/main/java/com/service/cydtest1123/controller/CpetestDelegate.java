142e1b354f4945c4e1c85eec41d363f38c78b38f
paas <paas@cse-codegen-backend-4193087237-2br1h>
2017-11-22 16:29:42 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/cydtest1123/controller/CpetestDelegate.java
@@ -0,0 +1,14 @@
+package com.service.cydtest1123.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CpetestDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
